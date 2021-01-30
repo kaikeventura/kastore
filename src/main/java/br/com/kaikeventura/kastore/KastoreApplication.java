06125ddcea6659a1d41a5435fac098b52a8142b8
kaikeventura <kaike.gnr@hotmail.com>
2021-01-30 20:36:32 -0300

    First commit

---
+++ src/main/java/br/com/kaikeventura/kastore/KastoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaikeventura.kastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kaikeventura/kastore/KastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kaikeventura.kastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
